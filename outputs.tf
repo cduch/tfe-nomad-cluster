05de67c5f515220fb5b97cca739066dab106c722
joestack <joern@joern-online.de>
2022-02-11 11:57:22 +0100

    renaming 3

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "nomad_server_private_ips" {
-  value = aws_instance.nomad_server.*.private_ip
+  value = aws_instance.server.*.private_ip
 }
 
 output "nomad_server_public_ips" {
-  value = aws_instance.nomad_server[*].public_ip
+  value = aws_instance.server[*].public_ip
 }
 
 output "nomad_client_private_ips" {
-  value = aws_instance.nomad_client.*.private_ip
+  value = aws_instance.client.*.private_ip
 }
 
 output "nomad_client_public_ips" {
-  value = aws_instance.nomad_client[*].public_ip
+  value = aws_instance.client[*].public_ip
 }
