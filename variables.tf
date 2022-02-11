7cc97dd3645aeb7dae792716c71cf982b43aecb2
joestack <joern@joern-online.de>
2022-02-11 12:01:44 +0100

    renaming 4

--- variables.tf
+++ variables.tf
@@ -94,7 +94,7 @@ variable "root_block_device_size" {
 
 variable "nomad_version" {
   description = "i.e. 1.2.5 or 1.2.5+ent"
-  default = "1.2.5+ent"
+  default = "1.2.6+ent"
 }
 
 variable "nomad_lic" {
